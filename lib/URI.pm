95a7b93d67775c85fcf60e4514e07e2971f897e9
Adam Prime <adam.prime@utoronto.ca>
2009-10-25 11:54:43 -0400

    Added a bunch of commentary on what's going on, and a README

--- lib/URI.pm
+++ lib/URI.pm
@@ -3,17 +3,26 @@ class URI {
     has $.opaque is rw;
     has $.fragment is rw;
 
+    #
+    # multi method constructors!
+    #
+    # this one is what get's called if you just pass in a string like:
+    # URI.new('mailto:lol@haha.com') 
+    #
     multi method new (Str $uri) {
-        my %h = %.parse_uri($uri);
+        my %h = self.parse_uri($uri);
         #
         # at this point it would make sense to look to see if there is a
         # subclass of URI that implements this scheme (by looking for
-        # URI::<scheme>, and then making one of them, instead of a generic
+        # URI::<scheme>), and then making one of them, instead of a generic
         # URI object
         #
         return self.bless(*,|%h);
     }
 
+    #
+    # this one gets called if you pass in a hash.
+    #
     multi method new (:$scheme, :$opaque, :$fragment) {
         return self.bless(*,:$scheme,:$opaque,:$fragment);
     }
--- lib/URI/http.pm
+++ lib/URI/http.pm
@@ -1,21 +1,40 @@
 use URI;
 use URI::Authority;
 
+#
+# for http type urls
+#
+
 class URI::http is URI {
     has URI::Authority $.authority is rw;
     has $.path is rw;
     has $.query is rw;
     has $.fragment is rw;
 
+    #
+    # i'm not really sure why this constructor has to be here, i would expect
+    # that it should be inherited from URI, and i'd think that it should just
+    # work, but if i take this out, i get:
+    #
+    #     too many arguments passed (2) - 1 param expected
+    #     in Main (file src/gen_setting.pm, line 295)
+    #
     multi method new (Str $uri) {
-        my %h = %.parse_uri($uri);
+        my %h = self.parse_uri($uri);
         return self.bless(*, |%h);
     }
 
+    #
+    # all strings, but an actually URI::Authority object.
+    #
     multi method new (:$scheme, URI::Authority :$authority, :$path, :$query, :$fragment) {
         return self.bless(*,:$scheme,:$authority,:$path,:$query,:$fragment);
     }
 
+    #
+    # if you pass in a string for the authority, we turn it into an URI::Authority
+    # object
+    #
     multi method new (:$scheme, :$authority, :$path, :$query, :$fragment) {
         return self.bless(*,
             :$scheme,
